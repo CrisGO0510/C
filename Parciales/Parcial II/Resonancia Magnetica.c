074451992ac555afb84d01affb1b0240f52aa522
CrisGO0510 <cristhiangiraldo053@gmail.com>
2023-03-29 21:08:17 -0500

    Miercoles 29 de marzo del 2023

--- Parciales/Parcial II/Resonancia Magnetica.c
+++ Parciales/Parcial II/Resonancia Magnetica.c
@@ -41,6 +41,7 @@ Plano en Z(Fotografia) | Líneas sospechosas x plano(foto) | Puntos sospechosos 
 // ? Prototipos de las funciones:
 int randomNum();
 void imp(int[100][100][100], int);
+int puntoSus(int [100][100][100], int, int, int);
 
 int main()
 {
@@ -67,6 +68,8 @@ int main()
 
     imp(resonancia, 0);
 
+    puntoSus(resonancia, 2, 2, 2);
+
     return 0;
 }
 
@@ -82,90 +85,137 @@ int randomNum()
 // ? Función que imprime la resonancia (Vector) en la hoja(Z)
 void imp(int vector[100][100][100], int z)
 {
-        // Imprimimos el valor de Z para guiarnos
-        printf("Z: %i\n\n    ", z);
+    // Imprimimos el valor de Z para guiarnos
+    printf("Z: %i\n\n    ", z);
 
-        // Sentencia for para imprimir las decenas del valor de la columna
-        for (int i = 0; i < 90; i++)
+    // Sentencia for para imprimir las decenas del valor de la columna
+    for (int i = 0; i < 90; i++)
+    {
+        // Cada 10 valores se le sumará 1 a la impresión
+        if (i % 10 == 0)
         {
-            // Cada 10 valores se le sumará 1 a la impresión
-            if (i % 10 == 0)
-            {
-                // Imprimimos el valor de las decenas
-                printf("         %i", i / 10 + 1);
-            }
-        } //! FIN DEL FOR PARA IMPRIMIR LAS DECENAS
-
-        printf("\n   ");
-
-        // Imprimimos el valor de las columnas
-        for (int i = 0; i < 10; i++)
-        {
-
-            printf("0123456789");
+            // Imprimimos el valor de las decenas
+            printf("         %i", i / 10 + 1);
         }
+    } //! FIN DEL FOR PARA IMPRIMIR LAS DECENAS
 
-        // Imprimimos la linea separadora entre los numeros y la resonancia
-        printf("\n  +----------------------------------------------------------------------------------------------------+\n");
+    printf("\n   ");
 
-        // Declaramos segunda sentencia for, para imprimir una fila(X) por cada iteración
-        for (int x = 0; x < 100; x++)
+    // Imprimimos el valor de las columnas
+    for (int i = 0; i < 10; i++)
+    {
+
+        printf("0123456789");
+    }
+
+    // Imprimimos la linea separadora entre los numeros y la resonancia
+    printf("\n  +----------------------------------------------------------------------------------------------------+\n");
+
+    // Declaramos segunda sentencia for, para imprimir una fila(X) por cada iteración
+    for (int x = 0; x < 100; x++)
+    {
+        // Imprimimos el valor de la fila(X)
+        printf("%i |", x % 10);
+
+        /* Sentencia for que me imprimirá uno a uno cada punto de la resonancia.
+        Se consideran sospechosos aquellos puntos para los que TODOS los puntos adyacentes
+        tengan un valor entre 20 y 40 (esto incluye los puntos pertenecientes al mismo plano,
+        al plano inferior y al plano superior). */
+        for (int y = 0; y < 100; y++)
         {
-            // Imprimimos el valor de la fila(X)
-            printf("%i |", x % 10);
-
-            /* Sentencia for que me imprimirá uno a uno cada punto de la resonancia.
-            Se consideran sospechosos aquellos puntos para los que TODOS los puntos adyacentes
-            tengan un valor entre 20 y 40 (esto incluye los puntos pertenecientes al mismo plano,
-            al plano inferior y al plano superior). */
-            for (int y = 0; y < 100; y++)
+            // TODO: Condicional para determinar si imprime "█" ó " ".
+            if (vector[x][y][z] >= 20 && vector[x][y][z] <= 40)
             {
-                //TODO: Condicional para determinar si imprime "█" ó " ".
-                if (vector[x][y][z] >= 20 && vector[x][y][z] <= 40)
-                {
-                    // Imprimimos que el punto es sospechoso "█"
-                    printf("█");
-                }
-                else
-                {
-                    // Imprimimos que el punto NO es sospechoso " "
-                    printf(" ");
-                }
-            } //! FIN DEL FOR PARA IMRPIMIR PUNTOS EN Y
-              // Imprimimos el valor de la fila(X)
-            printf("| %i\n", x % 10);
-        } //! FIN DEL FOR PARA IMPRIMIR LA LINEA X
-
-        // Imprimimos la linea separadora entre los numeros y la resonancia
-        printf("  +----------------------------------------------------------------------------------------------------+\n   ");
-
-        // Imprimimos el valor de las columnas
-        for (int i = 0; i < 10; i++)
-        {
-            printf("0123456789");
-        }
-
-        printf("\n   ");
-
-        // Sentencia for para imprimir las decenas del valor de la columna
-        for (int i = 0; i < 90; i++)
-        {
-            // Cada 10 valores se le sumará 1 a la impresión
-            if (i % 10 == 0)
-            {
-                // Imprimimos el valor de las decenas
-                printf("         %i", i / 10 + 1);
+                // Imprimimos que el punto es sospechoso "█"
+                printf("█");
             }
-        } //! FIN DEL FOR PARA IMPRIMIR LAS DECENAS
+            else
+            {
+                // Imprimimos que el punto NO es sospechoso " "
+                printf(" ");
+            }
+        } //! FIN DEL FOR PARA IMRPIMIR PUNTOS EN Y
+          // Imprimimos el valor de la fila(X)
+        printf("| %i\n", x % 10);
+    } //! FIN DEL FOR PARA IMPRIMIR LA LINEA X
 
-        printf("\n");
+    // Imprimimos la linea separadora entre los numeros y la resonancia
+    printf("  +----------------------------------------------------------------------------------------------------+\n   ");
+
+    // Imprimimos el valor de las columnas
+    for (int i = 0; i < 10; i++)
+    {
+        printf("0123456789");
+    }
+
+    printf("\n   ");
+
+    // Sentencia for para imprimir las decenas del valor de la columna
+    for (int i = 0; i < 90; i++)
+    {
+        // Cada 10 valores se le sumará 1 a la impresión
+        if (i % 10 == 0)
+        {
+            // Imprimimos el valor de las decenas
+            printf("         %i", i / 10 + 1);
+        }
+    } //! FIN DEL FOR PARA IMPRIMIR LAS DECENAS
+
+    printf("\n");
 }
 
 // ? Función que determina si un punto es sospechoso
-int puntoSus(int vector[100][100][100], int x, int y, int z){
+int puntoSus(int vector[100][100][100], int x, int y, int z)
+{
     /*
         Determinaremos si un punto es sospechoso sí alrededor de él hay los puntos están entre 20 y 40, tanto en la capa superior, actual e inferior.
         Es decir ni la hoja 0, ni la 99 se incluyen en este.
-        
     */
+
+    // Declaramos un variable que contará cuantos puntos adyacentes hay (deben haber 26)
+    int cont = 0;
+
+    // Primero hacemos las discriminantes
+
+    // Ni en la primera, ni en la ultima capa puede haber un punto sospechoso
+    if (z == 0 || z == 99)
+    {
+        return 0;
+    }
+
+    // Ni el la primera, ni en la ultima fila pueden haber puntos sospechosos
+    if (x == 0 || x == 99)
+    {
+        return 0;
+    }
+
+    // Ni el la primera, ni en la ultima columna pueden haber puntos sospechosos
+    if (y == 0 || y == 99)
+    {
+        return 0;
+    }
+
+    // Ahora verificaremos si la coordenada esta rodeada de puntos entre 20 y 40
+
+    for (int k = -1; k <= 1; k++)
+    {
+        for (int j = -1; j <= 1; j++)
+        {
+            for (int i = -1; i <= 1; i++)
+            {
+                // vector[x - i][y - j][z - k] = 40;
+
+                if (vector[x - i][y - j][z - k] >= 20 && vector[x - i][y - j][z - k] <= 40)
+                {
+                    cont++;
+                }
+            } //! FIN DEL FOR PARA ANALIZAR LOS PUNTOS EN X
+
+        } //! FIN DEL FOR PARA CAMBIAR EL VALOR DE Y
+
+    } //! FIN DEL FOR PARA CAMBIAR EL VALOR DE Z
+
+    printf("A: %i", cont);
+
+    return 0;
 }
