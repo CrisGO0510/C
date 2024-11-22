6fefb736d23ed8fa393e409f10f39481d1e4c8ac
CrisGO0510 <cristhiangiraldo053@gmail.com>
2024-11-22 09:53:25 +0000

    Finalizado

--- Estructura de Datos/proyecto final/main.c
+++ Estructura de Datos/proyecto final/main.c
@@ -16,27 +16,34 @@ int main()
   insertVertex(&(graph->vertices), 6);
   insertVertex(&(graph->vertices), 7);
   insertVertex(&(graph->vertices), 8);
+  insertVertex(&(graph->vertices), 9);
+  insertVertex(&(graph->vertices), 10);
 
   // Agregar arcos al grafo
-  inserDoubleEdge(&(graph->edges), 1, 2, 5);
+  inserDoubleEdge(&(graph->edges), 1, 2, 3);
   inserDoubleEdge(&(graph->edges), 1, 3, 2);
   inserDoubleEdge(&(graph->edges), 1, 4, 4);
 
   inserDoubleEdge(&(graph->edges), 2, 5, 2);
 
-  inserDoubleEdge(&(graph->edges), 3, 2, 1);
-  inserDoubleEdge(&(graph->edges), 3, 4, 1);
+  inserDoubleEdge(&(graph->edges), 3, 5, 3);
+  inserDoubleEdge(&(graph->edges), 3, 6, 5);
+  inserDoubleEdge(&(graph->edges), 3, 7, 4);
 
-  inserDoubleEdge(&(graph->edges), 4, 7, 5);
+  inserDoubleEdge(&(graph->edges), 4, 7, 3);
 
   inserDoubleEdge(&(graph->edges), 5, 6, 1);
   inserDoubleEdge(&(graph->edges), 5, 8, 3);
 
-  inserDoubleEdge(&(graph->edges), 6, 8, 2);
   inserDoubleEdge(&(graph->edges), 6, 7, 1);
+  inserDoubleEdge(&(graph->edges), 6, 10, 4);
 
-  inserDoubleEdge(&(graph->edges), 7, 8, 1);
+  inserDoubleEdge(&(graph->edges), 7, 9, 2);
 
+  inserDoubleEdge(&(graph->edges), 8, 9, 1);
+  inserDoubleEdge(&(graph->edges), 8, 10, 2);
+
+  inserDoubleEdge(&(graph->edges), 9, 10, 2);
 
   // // Recorrer la lista de vértices
   // VertexNode *a = getChildVertices(graph, 8);
@@ -47,8 +54,11 @@ int main()
 
   printf("INICIO\n\n");
 
-  VertexNode *a = BFS(graph, 1);
-  printVertices(a);
+  // VertexNode *a = BFS(graph, 1);
+  // printVertices(a);
+
+  EdgeNode *e = Kruskal(graph);
+  printEdges(e);
 
   printf("FIN\n");
 
